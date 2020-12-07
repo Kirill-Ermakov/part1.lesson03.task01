afc8c0b3e3011cfdece42982b5b6455bcc0ee237
Kirill-Ermakov <Liss_85@mail.ru>
2020-12-07 21:24:34 +0300

    First 2

--- src/VTB/ermakov/Main.java
+++ src/VTB/ermakov/Main.java
@@ -1,3 +1,5 @@
+
+
 package VTB.ermakov;
 
 public class Main {
@@ -8,6 +10,44 @@ public class Main {
 
         double money = price*volume;
         System.out.println(money);
+
+
+    }
+}
+
+
+Зарплата
+
+        package VTB.ermakov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int zp = 70000;
+        int nalog1 = 13;
+        int nalog2 = 100;
+        int money1 = zp/nalog2;
+        int money2 = money1*nalog1;
+        int zarplata = zp-money2;
+        System.out.println(zarplata);
+
+
+    }
+}
+
+Час
+        package VTB.ermakov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int vvodnaia = 3600;
+        int minuta = 60;
+        int sekund = 60;
+        int chas = minuta*sekund;
+        int itog = vvodnaia/chas;
+
+        System.out.println(itog);
 
 
     }
