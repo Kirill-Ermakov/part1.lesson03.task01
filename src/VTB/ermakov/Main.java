ed5f942805b8b6358dc61d52ec1a3cf3cafb178e
Kirill-Ermakov <Liss_85@mail.ru>
2020-12-07 21:20:26 +0300

    Initial commit

---
+++ src/VTB/ermakov/Main.java
@@ -0,0 +1,14 @@
+package VTB.ermakov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double price = 42.50;
+        int volume = 50;
+
+        double money = price*volume;
+        System.out.println(money);
+
+
+    }
+}
